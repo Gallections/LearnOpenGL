66334e16dd4199b7e15dd0847bee03f84442c185
Gallections <Gallections@gmail.com>
2026-02-04 23:52:49 -0800

    feat: implement vertex shader

---
+++ learnOpenGL/hello_triangle.cpp
@@ -0,0 +1,89 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// Graphics pipeline is usually divided into 2 large parts: the first transforms your 3D coordinates into 2D coordinates and the 
+// second part transforms the 2D coordinates into the actual colored pixels. 
+
+// The graphics pipeline takes as input a set of 3D coordinates and transforms these to colored 2D pixels on your screen. The graphics
+// can be divied into several steps where each step requries the output of the previous step as its input. All of these steps are highly 
+// specialized (they have one speciific function) and can easily be executed in parallel. Due to the parallel nature, graphics cards of today
+// have thousands of small processing cores to quickly process your data within the graphics pipeline. The processing cores run small programs on the GPU
+// for each step of the pipeline. These small programs are called "Shaders".
+
+// Shaders are written in OpenGL Shading Language (GLSL).
+
+GLFWwindow* initWindow(int width, int height) {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(width, height, "Hello Triangle", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return nullptr;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// Load GLAD (OpenGL function pointer)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return nullptr;
+	}
+
+	return window;
+}
+
+
+
+// OpenGL doesn't simply transform all your 3D coordinates to 2D pixels on your screen; OpenGL only processes 3D coordinates when 
+// they are in a specific range between -1.0 and 1.0 on all 3 axes (x, y, and z). All coordinates within this NDC (Normalized Device Coordinates) range 
+// will end up visible on your screen.
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f
+};
+
+
+int main() {
+	unsigned int VBO;
+	glGenBuffers(1, &VBO); // 1 specifies the number of buffer objects to generate. Asks OpenGL to generate a unique ID(name) for a new buffer object, and stores the ID number into the variable.
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// From this point on, any buffer calls we make (on the GL_ARRAY_BUFFER target) will be used to configure the currently bound buffer, which is VBO.
+	// Then we can make a call to the glBufferData function that copies the previously defined vertex data into the buffer's memory.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	// ========================== Creating a vertex shader ========================================
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout(location = 0) in vec3 aPos;\n"
+		"void main() {\n"
+		"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// The glShaderSource function takes the shader object to compile to its first argument. The second argument specifies how many strings we're passing as source code, 
+	// which is only one. The 3rd parameter is the actual source code of the vertex shader and we can leave the 4th paramter to NULL.
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512]; // allocates 512 bytes to the character array.
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	// the glGetShaderiv is an OpenGL function used to query specific information (paramters) about a compiled shader object, such as its compilation status. 
+
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+
+	return 0;
+}
